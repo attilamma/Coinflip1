ee7d2a9b7b3ca52e0dc35ee13c657706e3e4d761
attilamma <atk_91@yahoo.de>
2018-02-22 21:11:42 +0100

    added accelerometer and sound(sound bug)

--- app/src/main/java/com/example/attila/coinflip/MainActivity.java
+++ app/src/main/java/com/example/attila/coinflip/MainActivity.java
@@ -9,14 +9,37 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.view.animation.Animation;
-
 import java.util.Random;
+import android.media.MediaPlayer;
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    //Coinflip Values
     public static final Random RANDOM = new Random();
     private ImageView coin;
     private Button coinbutton;
+
+    //Acceleromenter Values
+    private float lastX, lastY, lastZ;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private float max = 0;
+
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    //Audio Value
+    MediaPlayer mp        = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +53,31 @@ public class MainActivity extends AppCompatActivity {
                 flipCoin();
             }
         });
+
+      //check accelerometer
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // success! we have an accelerometer
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        } else {
+            // no accelerometer, do nothing
+        }
+
     }
 
     private void flipCoin(){
+        mp = MediaPlayer.create(this, R.raw.coinflipaudio);
+        mp.start();
+
+        if (mp != null) {
+            mp.reset();
+            mp.release();
+        }
+
     Animation fadeOut = new AlphaAnimation(1,0);
         fadeOut.setInterpolator(new AccelerateInterpolator());
         fadeOut.setDuration(100);
@@ -63,4 +108,42 @@ public class MainActivity extends AppCompatActivity {
 
         ); coin.startAnimation(fadeOut);
     }
+
+    //onResume() register the accelerometer for listening the events
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    //onPause() unregister the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        // if the change is below 2, it is just plain noise
+        if (deltaX < 2)
+            deltaX = 0;
+        if (deltaY < 2)
+            deltaY = 0;
+        if (deltaZ < 2)
+            deltaZ = 0;
+
+        if (deltaX>15 || deltaY>15 || deltaZ>15){
+            flipCoin();
+        }
+    }
+
 }
